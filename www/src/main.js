e08dad016d4cc0a6a04789896af6949824424d50
cbdyzj <cbdyzj@jianzhao.org>
2022-01-21 19:02:30 +0800

    Add index_vue3.html

--- www/src/main.js
+++ www/src/main.js
@@ -1,6 +1,10 @@
 import { Vue } from './modules.js'
 import App from './App.js'
 
-const app = new Vue(App)
-
-app.$mount('#app')
+if (Vue.version.startsWith('2')) {
+    const app = new Vue(App)
+    app.$mount('#app')
+} else {
+    const app = Vue.createApp(App)
+    app.mount('#app')
+}
